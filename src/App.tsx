251f63104ea12cf480dd1ac48d89fcf0de4ea0cc
TheRPMko <123456rpmlp@gmail.com>
2024-04-01 13:55:46 +0200

    Initial framework

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="welcome-container">
+      <img className="main" src="/main-optimized.svg" />
+    </div>
+  );
+}
+
+export default App;
